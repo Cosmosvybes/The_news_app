e49df67d277d21201d30969b132efa2174821737
Cosmos <Alfredchrisayo@gmail.com>
2023-12-19 00:00:19 +0000

    add

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -15,7 +15,9 @@ const Footer = () => {
     <>
       <div className="flex h-auto bg-slate-950 justify-around px-2 py-2 max-sm:flex-col max-sm:h-auto">
         <div className="flex m-1  max-sm:w-auto flex-col">
-          <h1 className="underline text-white font-bold">Quick Links </h1>
+          <h1 className="underline text-white font-bold max-sm:text-sm">
+            Quick Links{" "}
+          </h1>
           <button className="text-left  text-white ">Help center</button>
           <button className="text-left  text-white ">How to earn</button>
           <button className="text-left  text-white ">
@@ -24,30 +26,34 @@ const Footer = () => {
         </div>
 
         <div className="flex justify-start flex-col m-1  max-sm:w-auto max-sm:justify-start ">
-          <h1 className="underline text-white font-bold ">Follow us </h1>
-          <button className="text-left  text-white ">
+          <h1 className="underline text-white max-sm:text-sm font-bold ">
+            Follow us{" "}
+          </h1>
+          <button className="text-left  text-white max-sm:text-sm ">
             <FaInstagram className="inline" /> Instagram
           </button>
-          <button className="text-left  text-white ">
+          <button className="text-left  text-white max-sm:text-sm ">
             <FaTwitter className="inline" /> X/Twitter
           </button>
-          <button className="text-left  text-white ">
+          <button className="text-left  text-white max-sm:text-sm ">
             <FaFacebook className="inline" /> FaceBook
           </button>
-          <button className="text-left  text-white ">
+          <button className="text-left  text-white max-sm:text-sm ">
             <FaWhatsapp className="inline" /> Whatsapp
           </button>
         </div>
 
         <div className="flex flex-col  max-sm:w-auto">
-          <h1 className="underline text-white font-bold ">Contact us </h1>
-          <button className="text-left  text-white ">
-            <FaPhone className="inline"/> +234(8140242752)
+          <h1 className="underline text-white max-sm:text-sm font-bold ">
+            Contact us{" "}
+          </h1>
+          <button className="text-left  text-white max-sm:text-sm ">
+            <FaPhone className="inline" /> +234(8140242752)
           </button>
-          <button className="text-left  text-white ">
-            <FaPhone className="inline"/> +234(9075848475)
+          <button className="text-left  text-white max-sm:text-sm ">
+            <FaPhone className="inline" /> +234(9075848475)
           </button>
-          <button className="text-left  text-white ">
+          <button className="text-left  text-white max-sm:text-sm ">
             <FaEnvelope className="inline" /> support@thenews.com
           </button>
         </div>
