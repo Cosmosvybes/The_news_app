d4e2747ed3ac250c57cd90562c9f39df183c5b4c
Cosmos <Alfredchrisayo@gmail.com>
2023-12-21 09:22:11 +0000

    add

--- src/Components/Post.jsx
+++ src/Components/Post.jsx
@@ -87,118 +87,109 @@ const Post = ({
   return (
     <>
       <Link to={`/post/${id}`}>
-         <div className="flex flex-center flex-col h-auto  px-2 rounded-sm  max-sm:w-full">
-        <div className="flex justify-between items-center">
-          <div className="flex justify-start items-center">
-            <div className="w-10 h-10 rounded-full border border-gray-500 py-1 px-1 flex justify-start items-center">
-              <img width="40px" height="40px" className="" />
+        <div className="flex flex-center flex-col h-auto  px-2 rounded-sm  max-sm:w-full">
+          <div className="flex justify-between items-center">
+            <div className="flex justify-start items-center">
+              <div className="w-10 h-10 rounded-full border border-gray-500 py-1 px-1 flex justify-start items-center">
+                <img width="auto" height="auto" className="" />
+              </div>
+              <p className="text-slate-900 rounded-xl text-sm  px-1">
+                {"User Name"}
+              </p>
+              <FaAtom className="text-sm text-sky-800" />
+              <p className="text-gray-400 text-sm m-0.5">{"@username"}</p>
+              <p className=" m-2  text-gray-500 flex justify-center items-center text-sm">
+                {" "}
+                <FaClock className="inline text-sky-500 px-1" />
+                {time}{" "}
+              </p>
             </div>
-            <p className="text-slate-900 rounded-xl text-sm  px-1">
-              {"User Name"}
-            </p>
-            <FaAtom className="text-sm text-sky-800" />
-            <p className="text-gray-400 text-sm m-0.5">{"@username"}</p>
-            <p className=" m-2  text-gray-500 flex justify-center items-center text-sm">
-              {" "}
-              <FaClock className="inline text-sky-500 px-1" />
-              {time}{" "}
-            </p>
+
+            <FaEllipsisV className="text-slate-400 text-sm " />
           </div>
 
-          <FaEllipsisV className="text-slate-400 text-sm " />
-        </div>
-
-        {/* //post body */}
-        <div className="flex px-10  h-auto">
-      
-            <p className="text-slate-900 text-sm text-justify ">
+          {/* //post body */}
+          <div className="flex px-10  h-auto ">
+            <p className="text-slate-900 text-sm text-justify border-2 border-gray-400 px-2 rounded-md ">
               {post}
             </p>
-         
-        </div>
+          </div>
 
-        {!openComment && (
-          <div className="flex px-10 py-1 justify-between items-center">
-            <div className="flex justify-start text-gray-600 items-center">
-              <FaComments className="text-sky-400 text-sm" />
-
-              <p className="text-sm  text-slate-900 "> {12}</p>
-            </div>
-            <div
-              onClick={() => likePost(id)}
-              className="flex text-red-600 justify-start items-center  hover:bg-slate-200 px-1 py-1 rounded-full"
-            >
-              <div className="flex items-center justify-start">
-                {" "}
-                <FaHeart className="text-sm " />
-                <p className="text-sm  text-slate-900  ">
-                  {" "}
-                  {numberOfLikes}
-                </p>
-              </div>
-            </div>
-            <div
-              onClick={() => firePost(id)}
-              className="flex text-yellow-600 justify-start items-center hover:bg-slate-200 px-1 py-1 rounded-full"
-            >
+          {!openComment && (
+            <div className="flex px-10 py-1 justify-between items-center">
               <div className="flex justify-start text-gray-600 items-center">
-                <FaFire className="text-yellow-600 text-sm" />
-                <p className="text-sm text-slate-900 ">
+                <FaComments className="text-sky-400 text-sm" />
+
+                <p className="text-sm  text-slate-900 "> {12}</p>
+              </div>
+              <div
+                onClick={() => likePost(id)}
+                className="flex text-red-600 justify-start items-center  hover:bg-slate-200 px-1 py-1 rounded-full"
+              >
+                <div className="flex items-center justify-start">
                   {" "}
-                  {fireNumber}
-                </p>
+                  <FaHeart className="text-sm " />
+                  <p className="text-sm  text-slate-900  "> {numberOfLikes}</p>
+                </div>
+              </div>
+              <div
+                onClick={() => firePost(id)}
+                className="flex text-yellow-600 justify-start items-center hover:bg-slate-200 px-1 py-1 rounded-full"
+              >
+                <div className="flex justify-start text-gray-600 items-center">
+                  <FaFire className="text-yellow-600 text-sm" />
+                  <p className="text-sm text-slate-900 "> {fireNumber}</p>
+                </div>
+              </div>
+              <div className="flex text-slate-400 justify-start items-center hover:bg-slate-200 px-1 py-1 rounded-full">
+                {" "}
+                <FaTag className="text-sm" />
               </div>
             </div>
-            <div className="flex text-slate-400 justify-start items-center hover:bg-slate-200 px-1 py-1 rounded-full">
-              {" "}
-              <FaTag className="text-sm" />
-            </div>
-          </div>
-        )}
+          )}
 
-        {openComment && (
-          <div className="grid h-auto gap-1 py-3 grid-cols-2 max-sm:grid-cols-2  max-sm:h-auto px-1 ">
-            <textarea
-              name=""
-              className="h-14 max-sm:h-10 border-2 border-gray-300 rounded-md "
-              id=""
-              cols="30"
-              rows="10"
-            ></textarea>
-            <button
-              onClick={() => setOpenComment(!openComment)}
-              className="bg-sky-500 w-18 py-1 rounded-md font-extrabold px-2 max-sm:px-1  text-white max-sm:text-sm"
-            >
-              {" "}
-              comment
-            </button>
-          </div>
-        )}
-      </div>
-      {openComment && (
-        <div className="comments w-full flex flex-col px-2 py-2 h-auto  border-b-2 border-slate-400 ">
-          {[1, 2, 3, 5, 6].map((comment) => {
-            return (
-              <div
-                className="flex flex-col justify-between bg-slate-200 px-1 py-2 m-0.2"
-                key={comment}
+          {openComment && (
+            <div className="grid h-auto gap-1 py-3 grid-cols-2 max-sm:grid-cols-1  max-sm:h-auto px-10 ">
+              <textarea
+                name=""
+                className="h-14 max-sm:h-12 border-2 border-gray-300 rounded-md "
+                id=""
+                cols="30"
+                rows="10"
+              ></textarea>
+              <button
+                onClick={() => setOpenComment(!openComment)}
+                className="bg-sky-500 w-18 py-1 rounded-md font-extrabold px-2 max-sm:px-1  text-white max-sm:text-sm"
               >
-                <div className="relative flex justify-start items-center">
-                  <img width={"10px"} height={"10px"} alt="image" />
-                  <p className="text-slate-900 px-1 max-sm:text-sm ">
-                    {"Username"}
+                {" "}
+                comment
+              </button>
+            </div>
+          )}
+        </div>
+        {openComment && (
+          <div className="comments w-full flex flex-col px-2 py-2 h-auto  border-b-2 border-slate-400 ">
+            {[1, 2, 3, 5, 6].map((comment) => {
+              return (
+                <div
+                  className="flex flex-col justify-between bg-slate-200 px-1 py-2 m-0.2"
+                  key={comment}
+                >
+                  <div className="relative flex justify-start items-center">
+                    <img width={"10px"} height={"10px"} alt="image" />
+                    <p className="text-slate-900 px-1 max-sm:text-sm ">
+                      {"Username"}
+                    </p>
+                  </div>
+                  <p className="text-slate-900 max-sm:text-sm ">
+                    {"The reponse from the mr ABsc "}
                   </p>
                 </div>
-                <p className="text-slate-900 max-sm:text-sm ">
-                  {"The reponse from the mr ABsc "}
-                </p>
-              </div>
-            );
-          })}
-        </div>
-      )}
-          </Link>
-     
+              );
+            })}
+          </div>
+        )}
+      </Link>
     </>
   );
 };
